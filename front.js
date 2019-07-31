ba4e90588ec04240c68b3b29c4387876f25df202
Austin <austin.dober@gmail.com>
2019-07-30 21:15:59 -0400

    change HTML

--- front.js
+++ front.js
@@ -1,4 +1,4 @@
-var start = document.getElementById("stYr").value = yyyy;
+var start = document.getElementById("stYr");
 var end = document.getElementById("edYr");
 var searchTerms = document.getElementById("term");
 var NumbRec = document.getElementById("rec");
