0b77768e84951668785118709127d6cbd937f2a9
Austin <austin.dober@gmail.com>
2019-07-30 20:54:36 -0400

    adding front end javascript

---
+++ front.js
@@ -0,0 +1,4 @@
+var start = document.getElementById("stYr");
+var end = document.getElementById("edYr");
+var searchTerms = document.getElementById("term");
+var NumbRec = document.getElementById("rec");
